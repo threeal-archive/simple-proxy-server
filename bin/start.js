90719458019ad9e7ff501b594aaac221a1a56b8a
Alfi Maulana <alfi.maulana.f@gmail.com>
2020-09-19 19:46:55 +0700

    add ssl support (unfinished)

--- bin/start.js
+++ bin/start.js
@@ -1,15 +1,42 @@
 const http = require('http');
+const https = require('https');
+
+const fs = require('fs');
 
 let host = process.argv[2] || 'google.com';
 let port = process.argv[3] || 8080;
 
-http.createServer((request, response) => {
+let key, cert;
+try {
+  key = fs.readFileSync('./ssl/ssl.key');
+  cert = fs.readFileSync('./ssl/ssl.crt');
+}
+catch {
+  console.log('No SSL key and/or certificate provided, using HTTP instead');
+}
+
+let protocol = http;
+let options = {};
+if (key) {
+  if (cert) {
+    protocol = https;
+    options = {
+      key: key,
+      cert: cert,
+    };
+  }
+}
+
+protocol.createServer(options, (request, response) => {
   Object.entries(request.headers).forEach((header) => {
     let key = header[0];
     if (key.toUpperCase() == 'IF-NONE-MATCH') {
       delete request.headers[key];
+    } else if (key.toUpperCase() == 'HOST') {
+      // request.headers[key] = host;
     }
   });
+
   let body = [];
   request.on('error', (error) => {
     console.log(error);
@@ -22,7 +49,8 @@ http.createServer((request, response) => {
       method: request.method,
       headers: request.headers,
     };
-    http.request(options, (res) => {
+
+    protocol.request(options, (res) => {
       let body = [];
       res.on('data', (chunk) => {
         body.push(chunk);
